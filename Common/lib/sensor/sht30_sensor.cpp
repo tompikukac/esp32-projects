368124cbf40cac5fe698121b0388932393ba058d
tompikukac <tompikukac@gmail.com>
2026-02-10 22:49:53 +0100

    sht30 tweak

--- Common/lib/sensor/sht30_sensor.cpp
+++ Common/lib/sensor/sht30_sensor.cpp
@@ -5,16 +5,19 @@ SHT30Sensor::SHT30Sensor(TwoWire& wire, uint8_t address)
 
 bool SHT30Sensor::beginImpl() {
   wire.begin(5, 6);  // SDA=GPIO5, SCL=GPIO6
-
+  Wire.setClock(100000);
   if (!sht.begin(address)) {
     return false;
   }
-
   return true;
 }
 
 TemperatureData SHT30Sensor::readImpl() {
   TemperatureData data;
+
+  sht.readTemperature();
+  sht.readHumidity();
+  delay(50);
 
   float t = sht.readTemperature();
   float h = sht.readHumidity();
